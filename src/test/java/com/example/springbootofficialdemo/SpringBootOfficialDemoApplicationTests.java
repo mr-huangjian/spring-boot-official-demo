3ffd1cac5a1c5657bf7552180ae53d46faedac46
mr-huangjian <mr.huangjian@foxmail.com>
2022-09-25 01:31:24 +0800

    Add files via upload

---
+++ src/test/java/com/example/springbootofficialdemo/SpringBootOfficialDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootofficialdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOfficialDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
